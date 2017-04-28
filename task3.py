9abcafd56c6e031072c42b82521a32fe179a62eb
Luc Pitre <pitreluc@gmail.com>
2017-04-27 22:16:19 -0400

    Create task3.py

---
+++ task3.py
@@ -0,0 +1,10 @@
+"""
+Your First and Last Name
+Date : day month year
+
+ASSIGNMENT DETAILS
+> create a program that asks the user whether it is raining, snowing, windy or sunny outside
+> using conditional statements, determine what you will say to the user using the print function
+
+*assignment goes underneath these quote marks*
+"""
