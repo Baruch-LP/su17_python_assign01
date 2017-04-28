665a7717e807803ed1b2d131c3e3caa4a7daded8
Luc Pitre <pitreluc@gmail.com>
2017-04-27 22:14:29 -0400

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,11 @@
+"""
+Your First and Last Name
+Date : day month year
+
+ASSIGNMENT DETAILS
+> Write a program that asks the user two questions such as their name, age, favortie TV show, etc. 
+> Create a simple dialogue using the information gathered such as: 
+    Hi, John! I heard you liked Shark Tank a lot. There's a new episode this week.
+
+*assignment goes underneath these quote marks*
+"""
