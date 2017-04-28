8c3736b04b00706917b8c7d8384cef680f8ed7ab
Luc Pitre <pitreluc@gmail.com>
2017-04-27 22:13:38 -0400

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,10 @@
+"""
+Your First and Last Name
+Date : day month year
+
+ASSIGNMENT DETAILS
+> write a program that asks the user to enter three numbers
+> using the three numbers from the user, find the sum, average, and product.
+
+*assignment goes underneath these quote marks*
+"""
